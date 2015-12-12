5cb28f723685136f264b1794f7d448a6cf5ce7df
Todd Jeffreys <todd@voidpointer.org>
2015-12-12 14:00:33 -0700

    Remove copyright comments

--- GamecubeFilesystemTable.h
+++ GamecubeFilesystemTable.h
@@ -1,15 +1,3 @@
-/*
- * fs/gcdvdfs/fst.h
- *
- * Nintendo GameCube Filesystem driver
- * Copyright (C) 2006 The GameCube Linux Team
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- */
 #ifndef __fst__h
 #define __fst__h
 
